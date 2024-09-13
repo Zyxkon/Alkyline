d96aacefd14fd465ae1f136f56db21a4af95748b
Zyxkon <quangglider@gmail.com>
2024-09-14 00:06:02 +0700

    fixing

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from alkyline import Alkyline
 from json import load
+import asyncio
 with open("config.json", "r") as config:
     conf = load(config)
 if __name__ == "__main__":
     alkyline = Alkyline(config=conf)
-    alkyline.load_all_extensions()
+    asyncio.run(alkyline.load_all_extensions())
     alkyline.run()
